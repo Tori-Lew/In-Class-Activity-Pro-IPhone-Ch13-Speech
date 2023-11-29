18305ce72311cca35b49e4a7fb860a4035f9d6ce
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-11-29 15:58:20 -0800

    Initial commit

---
+++ In-Class Activity Pro IPhone Ch13 Speech/In_Class_Activity_Pro_IPhone_Ch13_SpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch13_SpeechApp.swift
+//  In-Class Activity Pro IPhone Ch13 Speech
+//
+//  Created by Student Account on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch13_SpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
